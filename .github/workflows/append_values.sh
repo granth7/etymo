b2b1f0b609d064e9dfc582e6d774a4ffefe5e3cc
granth7 <gah516@gmail.com>
2024-10-16 12:37:12 -0700

    Add steps to build workflow for appending values and updating chart.

---
+++ .github/workflows/append_values.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the values to append
+VALUES="
+expose:
+  webfrontend:
+    ingress:
+      enabled: false
+      ingressClassName: internal
+      annotations: {}
+      labels: {}
+      path: /
+      pathType: Prefix
+      hosts: [\"etymo.your.domain\"]
+"
+
+# Append the values to the existing values.yaml file
+echo "$VALUES" >> values.yaml
+
+echo "Values appended to values.yaml"
