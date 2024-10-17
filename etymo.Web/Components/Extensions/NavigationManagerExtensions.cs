91159c4022364c52b091ade64890fce9ddbea6b9
granth7 <gah516@gmail.com>
2024-10-17 13:55:11 -0700

    Fix bug and visual glitch that occured on refresh.

---
+++ etymo.Web/Components/Extensions/NavigationManagerExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace etymo.Web.Components.Extensions
+{
+    public static class NavigationManagerExtensions
+    {
+        public static void RemoveQueryParameters(this NavigationManager navManager)
+        {
+            var uri = new Uri(navManager.Uri);
+            var newUri = uri.GetLeftPart(UriPartial.Path); // Removes query parameters
+            navManager.NavigateTo(newUri, forceLoad: false);
+        }
+
+
+        public static void RemoveFragment(this NavigationManager navManager)
+        {
+            var uri = new Uri(navManager.Uri);
+            if (!string.IsNullOrEmpty(uri.Fragment))
+            {
+                var newUri = uri.GetLeftPart(UriPartial.Path); // Remove fragment
+                navManager.NavigateTo(newUri, forceLoad: false);
+            }
+        }
+    }
+}
