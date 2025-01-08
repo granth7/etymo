eeb0c56535047c140204b1c9a8a22e5f7f0f0a08
granth7 <gah516@gmail.com>
2025-01-07 16:50:07 -0800

    Add basic postgres integration to connect to existing db.

---
+++ etymo.ApiService/MyEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace etymo.ApiService
+{
+    public class MyEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedDate
+        {
+            get; set;
+        }
+    }
+}
--- etymo.AppHost/Program.cs
+++ etymo.AppHost/Program.cs
@@ -1,12 +1,31 @@
 var builder = DistributedApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__existingPostgres"); 
+
+// If the environment variable exists, then the app has been deployed to a cluster. So, connect to the database running in the cluster.
+if (connectionString != null)
+{
+    configuration["ConnectionStrings:existingPostgres"] = connectionString;
+}
+// If the environment variable doesn't exist, then the app is running locally. Enter the credentials (do not commit) or create a local environment variable. 
+// Example: setx ConnectionStrings__existingPostgres "Host=myserver;Database=mydb;Username=myuser;Password=mypassword"
+// Reopen Visual Studio to pick up the changes.
+else if (connectionString == null)
+{
+    configuration["ConnectionStrings:existingPostgres"] = "Host=myserver;Database=mydb;Username=myuser;Password=mypassword";
+}
 
 var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.etymo_ApiService>("etymo-apiservice");
 
+var existingPostgres = builder.AddConnectionString("existingPostgres");
+
 builder.AddProject<Projects.etymo_Web>("etymo-webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithReference(cache)
-    .WithReference(apiService);
+.WithExternalHttpEndpoints()
+.WithReference(cache)
+.WithReference(apiService)
+.WithReference(existingPostgres);
 
 builder.Build().Run();
