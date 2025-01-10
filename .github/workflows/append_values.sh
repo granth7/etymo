bcc4c62c3aa46051265cf0278085c4a9a4e11dba
granth7 <gah516@gmail.com>
2025-01-09 17:38:55 -0800

    Add split function back.

--- .github/workflows/append_values.sh
+++ .github/workflows/append_values.sh
@@ -14,9 +14,7 @@ expose:
       hosts: [\"etymo.your.domain\"]
 secrets:
   connectionStrings:
-    existingPostgres:
-      name: \"secret-name\"
-      key: \"secret-key\"
+    existingPostgres: \"secret-name/secret-key\"
 "
 
 # Append the values to the existing values.yaml file
